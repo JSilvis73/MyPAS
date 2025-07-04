d2e7ec631a30a2261879686d277281f35f877ffa
JSilvis <SilvisDevelopment@gmail.com>
2025-07-03 23:53:12 -0400

    Updated DBFactory to use configuration file variables.

--- server/MyPAS/Data/MyPASContextFactory.cs
+++ server/MyPAS/Data/MyPASContextFactory.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace MyPAS.Data
 {
@@ -7,10 +9,13 @@ namespace MyPAS.Data
     {
         public MyPASContext CreateDbContext(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
             var optionsBuilder = new DbContextOptionsBuilder<MyPASContext>();
-
-            optionsBuilder.UseSqlServer("Server=localhost;Database=MyPASDBProd;Trusted_Connection=True;TrustServerCertificate=True;");
-
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
 
             return new MyPASContext(optionsBuilder.Options);
         }
