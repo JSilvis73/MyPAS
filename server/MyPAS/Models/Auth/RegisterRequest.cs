b11c9f4ca697bfd52fcbbdca38867fd4969f286a
JSilvis <SilvisDevelopment@gmail.com>
2025-07-05 00:58:18 -0400

    Finished RegisterRequest model.

--- server/MyPAS/Models/Auth/RegisterRequest.cs
+++ server/MyPAS/Models/Auth/RegisterRequest.cs
@@ -6,5 +6,16 @@ namespace MyPAS.Models.Auth
     {
         [Required]
         [Email]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public string Password { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
     }
 }
