e49d929607357f168ef93543c862fde67d11aedc
JSilvis <SilvisDevelopment@gmail.com>
2025-09-24 17:49:22 -0400

    Created an interface for patient services. Creating the implementation next.

---
+++ server/MyPAS/Interfaces/IPatientServices.cs
@@ -0,0 +1,12 @@
+
+public interface IPatientServices
+{
+    Patient GetPatientById(int id);
+    IEnumerable<Patient> GetAllPatients();
+
+    Patient CreatePatient(Patient patient);
+    void UpdatePatientById(Patient updatedPatient);
+    void DeletePatientById(int id);
+
+    
+}
