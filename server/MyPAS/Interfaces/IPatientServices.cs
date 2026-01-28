5f18d90fff89080e6aabc504accff90f5040fb9a
JSilvis <SilvisDevelopment@gmail.com>
2026-01-27 20:38:08 -0500

    Modifying Service

--- server/MyPAS/Interfaces/IPatientServices.cs
+++ server/MyPAS/Interfaces/IPatientServices.cs
@@ -1,4 +1,4 @@
-
+using MyPAS.Models;
 
 public interface IPatientService
 {
--- server/MyPAS/Services/PatientServices.cs
+++ server/MyPAS/Services/PatientServices.cs
@@ -1,68 +1,36 @@
-//using MyPAS.Models;
+using MyPAS.Models;
 
-//public class PatientServices //: IPatientServices
-//{
-//    MyPASContext _context;
+public class PatientService : IPatientService
+    {
+        private readonly MyPASContext _context;
+    
+        public PatientService(MyPASContext context)
+        {
+          _context = context;
+        }
 
-//    public PatientServices(MyPASContext context)
-//    {
-//        _context = context;
-//    }
+    public IEnumerable<Patient> GetAll() 
+    {
+        return _context.Patients.ToList();
+    }
 
+    public Patient GetById(int id) 
+    {
+        var patientToFind = _context.Patients.FirstOrDefault(p => p.Id == id);
 
-//    // Find All Patients
+        return patientToFind
+            ?? throw new InvalidOperationException("Patient Not Found");
+        
+    }
 
-//    // Find Patient by Id
-//    public Patient GetPatientById(int id)
-//    {
-//        var patient = _context.Patients.Find(id);
-//        if (patient == null) { return null; }
-//        return patient;
-//    }
-//    // Create Patient
-//    public Patient CreatePatient(Patient patient)
-//    {
-//        _context.Patients.Add(patient);
-//        return patient;
-//    }
+    public Patient CreatePatient(Patient patient)
+    {
 
-//    // Update Patient
-//    public Patient UpdatePatient(Patient patient)
-//    {
-//        Patient patientToUpdate = _context.Patients.Find(patient.Id);
-//        if (patientToUpdate == null) { return patient; }
+    }
 
-//        patientToUpdate.FirstName = patient.FirstName;
-//        patientToUpdate.LastName = patient.LastName;
-//        //patientToUpdate.DateOfBirth = patient.DateOfBirth;
-//        patientToUpdate.Address = patient.Address;
-//        patientToUpdate.City = patient.City;
-//        patientToUpdate.State = patient.State;
-//        patientToUpdate.Zip = patient.Zip;
-//        patientToUpdate.Age = patient.Age;
-//        patientToUpdate.Phone = patient.Phone;
-//        patientToUpdate.Email = patient.Email;
+    public void DeletePatient(int id)
+    {
 
-//        _context.Patients.Update(patientToUpdate);
-//        _context.SaveChanges();
+    }
 
-//        return patientToUpdate;
-//    }
-
-//    // Delete Patient 
-//    public void DeletePatient(int id)
-//    {
-//        Patient patientToDelete = _context.Patients.Find(id);
-//        if (patientToDelete == null) { return; }
-
-//        // Logic to handle related Services and Payments if necessary
-//        var relatedServices = _context.Services.Where(s => s.PatientId == id);
-//        _context.Services.RemoveRange(relatedServices);
-
-//        var relatedPayments = _context.Payments.Where(p => p.PatientId == id);
-//        _context.Payments.RemoveRange(relatedPayments);
-
-//        _context.Patients.Remove(patientToDelete);
-//        _context.SaveChanges();
-//    }
-//}
+}
