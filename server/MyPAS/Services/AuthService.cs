13f8a160c9aa58fa3529c5b11691d946c3240a80
JSilvis <SilvisDevelopment@gmail.com>
2026-05-23 13:25:10 -0400

    Creating AuthService.

---
+++ server/MyPAS/Services/AuthService.cs
@@ -0,0 +1,17 @@
+﻿using MyPAS.Interfaces;
+
+namespace MyPAS.Services
+{
+    public class AuthService : IAuthService
+    {
+        public void Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
