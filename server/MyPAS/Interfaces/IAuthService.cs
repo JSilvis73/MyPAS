a03770a207110b30034752a859364ffbcf5a9211
JSilvis <SilvisDevelopment@gmail.com>
2026-05-23 13:22:55 -0400

    Creating an interface for authservice.:

---
+++ server/MyPAS/Interfaces/IAuthService.cs
@@ -0,0 +1,8 @@
+﻿namespace MyPAS.Interfaces
+{
+    public interface IAuthService
+    {
+        void Login(string username, string password);
+        void Register(string username, string password);
+    }
+}
