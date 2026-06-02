9042688ea1653d92722fda7db8ea8a8b8c6f9ab5
JSilvis <SilvisDevelopment@gmail.com>
2026-06-02 13:38:25 -0400

    Refactored the register endpoint in the auth controller. It is working.

--- server/MyPAS/Controllers/AuthController.cs
+++ server/MyPAS/Controllers/AuthController.cs
@@ -47,87 +47,17 @@ namespace MyPAS.Controllers
                 Ok(result):
                 Unauthorized(result.Errors);
 
-            //// Model is valid, proceed with login.
-            //var user = await _userManager.FindByEmailAsync(loginDto.Email);
-            //var userEmail = user?.Email;
-
-            //// Check: user is valid, ensure password matches, and user email is populated.
-            //if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password) || userEmail == null)
-            //{
-            //    _logger.LogWarning("Login failed for user: {email}.", loginDto.Email);
-            //    return Unauthorized(new { message = "Invalid email or password." });
-            //}
-
-            //// Assemble the user.
-            //var result = await _signInManager.PasswordSignInAsync(user, loginDto.Password, isPersistent: false, lockoutOnFailure: false);
-
-            //// Create claims and sign in the user.
-            //if (!result.Succeeded)
-            //{
-            //    _logger.LogWarning("Login failed for user: {email}.", loginDto.Email);
-            //    return Unauthorized(new { message = "Invalid login attempt." });
-            //}
-
-            //// Upon login in generate JWT.
-            //var token = _jwtService.GenerateToken(user.Id, userEmail);
-
-            //// If token is valid, issue token.
-            //if (token != null)
-            //{
-            //    _logger.LogInformation("User {email} logged in successfully.", loginDto.Email);
-            //    return Ok(new { token, user = new { user.Id, user.Email, user.FirstName, user.LastName } });
-            //}
-               
-            //// If we got here, login was unsuccessful.
-            //    _logger.LogWarning("Login failed for user: {email}.", loginDto.Email);
-            //return Unauthorized(new { message = "Invalid login attempt" });
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] AuthRegisterRequest request)
         {
-            // Log information to file.
-            _logger.LogInformation("Attempting to register user: {email}.", request.Email);
 
-            if (!ModelState.IsValid)
-            {
-                _logger.LogWarning("Invalid registration attempt for user: {email}.", request.Email);
-                return BadRequest(ModelState);
-            }
+            var result = await _authService.Register(request);
 
-            // Model is valid, proceed with registration.
-            _logger.LogInformation("Registering user {email} with first name {firstName} and last name {lastName}.",
-                request.Email, request.FirstName, request.LastName);
-
-            // Build new user in memory.
-            var user = new MyPASUser
-            {
-                UserName = request.Email,
-                Email = request.Email,
-                FirstName = request.FirstName,
-                LastName = request.LastName,
-            };
-
-            // Attempt to create user.
-            var result = await _userManager.CreateAsync(user, request.Password);
-
-            if (result.Succeeded)
-            {
-                _logger.LogInformation("User {email} successfully registered.", request.Email);
-                return Ok(new { message = "User registered successfully" });
-            }
-
-            // If we got here, something failed.
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError(error.Code, error.Description);
-            }
-
-            _logger.LogWarning("Registration failed for user: {email}.", request.Email);
-
-            return BadRequest(ModelState);
-
-
+            return (result.Success)? 
+                    Ok(result): 
+                    Unauthorized(result.Errors);
 
         }
 
