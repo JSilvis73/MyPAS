0d6687406b8875f769ea82ceeeefb64f47510193
JSilvis <SilvisDevelopment@gmail.com>
2026-06-06 13:29:50 -0400

    Refactored patient controller endpoints.

--- server/MyPAS/Controllers/PatientsController.cs
+++ server/MyPAS/Controllers/PatientsController.cs
@@ -32,102 +32,38 @@ namespace MyPAS.Controllers
         // Get specific patient by ID.
         [HttpGet("{id}")]
         public async Task<ActionResult<Patient>> GetPatientById(int id)
-        {
-         
-            
-            try
-            {
-                var patient =  await _patientService.GetPatientById(id);
-                if (patient == null) { return NotFound(); }
+        { 
 
-           
-                return Ok(patient);
-   
-            }
-            catch (Exception ex)
-            {
-               
-                return StatusCode(500, " An internal server error occurred.");
-            }
+            var patient =  await _patientService.GetPatientById(id);
+            if (patient == null) { return NotFound(); }
+            return Ok(patient);
         }
 
         // Create patient
-        //[HttpPost]
-        //public async Task<ActionResult> AddPatient([FromBody] Patient patient)
-        //{
-            
-
-        //    try
-        //    {
-        //        if (patient == null)
-        //        {
-                  
-        //            return BadRequest("Patient could not be added.");
-        //        }
-        //        await _patientService.CreatePatient(patient.FirstName, patient.LastName);
-
-             
-        //        return CreatedAtAction(nameof(GetPatientById), new { id = patient.Id }, patient);
-        //    }
-        //    catch (Exception ex)
-        //    {
-               
-        //        return StatusCode(500, "An internal server error occurred.");
-        //    }
-        //}
+        [HttpPost]
+        public async Task<ActionResult> AddPatient([FromBody] CreatePatientDTO createPatientDTO)
+        {
+            var result = await _patientService.CreatePatient(createPatientDTO);
+            if (result == null) return NotFound();
+            return Ok(result);
+        }
 
         // Update patient
-        //[HttpPut("{id}")]
-        //public async Task<ActionResult> UpdatePatient(int id, [FromBody] Patient patientToUpdate)
-        //{
-           
-        //        patientToUpdate.LastName, patientToUpdate.FirstName);
-
-        //    try
-        //    {
-        //        if (patientToUpdate == null) return BadRequest();
-        //        await _patientService.UpdatePatient(patientToUpdate);
-                
-               
-        //        return Ok(patientToUpdate);
-        //    }
-        //    //catch (DbUpdateConcurrencyException ex)
-        //    //{
-        //    //    _logger.LogError(ex, $"Concurrency error while updating patient ID: {id}");
-        //    //    if (!_context.Patients.Any(p => p.Id == id))
-        //    //    {
-        //    //        return NotFound();
-        //    //    }
-
-        //    //    throw; // optional: rethrow if you're in dev and want to crash upward
-        //    //}
-        //    catch (Exception ex)
-        //    {
-                
-        //        return StatusCode(500, "An internal server error occurred.");
-        //    }
-        //}
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdatePatient(int id, UpdatePatientDTO updatePatientDTO)
+        {
+            var result = await _patientService.UpdatePatient(id, updatePatientDTO);
+            if (result == null) return NotFound();
+            return Ok(result);
+        }
 
 
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeletePatient(int id)
         {
-            
-
-            try
-            {
-                await _patientService.DeletePatient(id);
-
-               
-                return NoContent();
-            }
-            catch (Exception ex) 
-            {
-                
-                return StatusCode(500, "An internal server error occurred.");
-            }
-
+            var result = await _patientService.DeletePatient(id);
+            if (!result) return NotFound();
+            return NoContent();
         }
-
     }
 }
