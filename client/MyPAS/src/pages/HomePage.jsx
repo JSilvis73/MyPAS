19f2b10e3d184d9e82c1e362e345dbdbbf08aff4
Jason <SilvisDevelopment@Gmail.com>
2026-07-10 14:04:42 -0400

    Last commit before backend service modification.

--- client/MyPAS/src/pages/HomePage.jsx
+++ client/MyPAS/src/pages/HomePage.jsx
@@ -3,13 +3,14 @@ import Card from "../components/Card";
 import { CiSearch } from "react-icons/ci";
 import { IoCreateOutline } from "react-icons/io5";
 import { FaTasks } from "react-icons/fa";
+import { useAuth } from "../context/AuthContext";
 
 
 export default function HomePage() {
   return (
     <div className="w-3xl max-w-6xl mx-auto bg-gray-800 text-white text-center border border-gray-600 rounded-lg p-4 shadow-lg">
       <h2 className="text-2xl mb-4"><strong>Home</strong></h2>
-      <p>Welcome </p>
+      <p>Welcome {useAuth()?.user?.username}</p>
      
         <div className="flex justify-center flex-wrap space-x-4 gap-4 mt-4">
           <Card props={{route: "/search", name: "Search", img: <CiSearch className="h-20 w-20" />}} />
