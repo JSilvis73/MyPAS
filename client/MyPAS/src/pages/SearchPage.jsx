e36ffa84871e825443905dabd77db57ceec0124c
Jason <SilvisDevelopment@Gmail.com>
2026-07-13 15:37:03 -0400

    Redesigned the search page: Added a clear results button as well as changed the layout to grid.

--- client/MyPAS/src/pages/SearchPage.jsx
+++ client/MyPAS/src/pages/SearchPage.jsx
@@ -11,6 +11,8 @@ export default function SearchPage() {
     lastName: "",
     firstName: "",
   });
+ 
+
   const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
   // Get List of patients initially and refresh during search.
@@ -49,19 +51,24 @@ export default function SearchPage() {
   };
 
   // Reset form data.
-  const handleClear = () => {
+  const handleClearSearchOptions = () => {
     setSearchOptions({ patientId: "", lastName: "", firstName: "" });
     setFilteredPatients(patients);
   };
 
+  const handleClear = () => {
+    setFilteredPatients([]);
+  }
+
+
   // Display
   return (
-    <div>
-      <div className="max-w-6xl mx-auto bg-gray-800 text-white border border-gray-600 rounded-lg p-6 shadow-lg text-center ">
+    <div className=" m-2  text-center">
+      <div className="max-w-6xl mx-auto bg-gray-800 text-white border border-gray-600 rounded-lg p-6 shadow-lg  ">
         <div className="m-2">
-          <h2 className="text-2xl mb-4">
+          <h1 className="text-2xl font-bold mb-4">
             <strong>Search</strong>
-          </h2>
+          </h1>
 
           <form
             className="flex flex-wrap gap-2 items-center justify-center"
@@ -105,31 +112,43 @@ export default function SearchPage() {
               <FaSearch className="inline mr-1" />
               Search
             </button>
+                    <button
+          className="p-1 mt-4 border rounded-lg hover:bg-red-500 hover:text-black"
+          onClick={handleClearSearchOptions}
+        >
+          Clear
+        </button>
           </form>
         </div>
       </div>
 
       <div className="max-w-6xl flex flex-col items-center mx-auto bg-gray-800 text-white border border-gray-600 rounded-lg p-6 shadow-lg m-4">
-        <h1 className="m-2 text-lg">Search Results:</h1>
-        <p className="m-2 text-sm">
+        <h1 className="text-2xl font-bold mb-4">Search Results:</h1>
+        
+        <div className="m-2 flex flex-col gap-2 items-center justify-center">
+        <p className="m-2 font-bold ">
           Found {filteredPatients.length} match
           {filteredPatients.length === 1 ? "" : "es"}
         </p>
-        <button
-          className="m-2 border p-1 rounded-md hover:bg-white hover:text-black"
-          onClick={handleClear}
-        >
-          Clear
+        <button type="button" className="p-1 border rounded-lg hover:text-black hover:bg-red-500"
+          onClick={handleClear}>
+              Clear Results
         </button>
+       </div>
 
+       {/* Display the filtered patients in a grid layout with links to their individual pages.*/}
+        <div className="m-2 grid md:grid-cols-2 lg:grid-cols-3 gap-2">
         {filteredPatients.map((patient) => (
           <Link key={patient.id} to={`/patients/${patient.id}`}>
-            <div className="px-6 py-3 rounded-xl m-2 bg-white/10 border border-white/30 backdrop-blur-lg text-white font-semibold shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] hover:bg-white/20 hover:scale-105 transition-all duration-300 ease-in-out">
-              {patient.id}: {patient.lastName}, {patient.firstName}
+            <div className="p-2 rounded-xl  bg-white/10 border border-white/30 backdrop-blur-lg text-white font-semibold shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] hover:bg-white/20 hover:scale-105 transition-all duration-300 ease-in-out">
+              Id: {patient.id}: {patient.lastName}, {patient.firstName}
             </div>
           </Link>
         ))}
+         </div>
       </div>
+
     </div>
+
   );
 }
