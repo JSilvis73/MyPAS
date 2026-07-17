74990801e23be89becceb7b04c0d3ececa6fd859
Jason <SilvisDevelopment@Gmail.com>
2026-07-17 15:37:29 -0400

    Changed passwordchange dto.

--- server/MyPAS/Models/Auth/ChangePasswordDTO.cs
+++ server/MyPAS/Models/Auth/ChangePasswordDTO.cs
@@ -5,6 +5,5 @@
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string NewPassword { get; set; } = string.Empty;
-        public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
