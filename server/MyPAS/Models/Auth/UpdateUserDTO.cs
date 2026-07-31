208fdc710cf21f48a9bff07b13654b72f7bdb362
Jason <SilvisDevelopment@Gmail.com>
2026-07-31 11:50:05 -0400

    Undid the update of the user model.

--- server/MyPAS/Models/Auth/UpdateUserDTO.cs
+++ server/MyPAS/Models/Auth/UpdateUserDTO.cs
@@ -11,8 +11,5 @@
 
         public string Phone { get; set; } = string.Empty;
 
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
-        public bool IsActive { get; set; } = true;
     }
 }
