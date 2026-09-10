d0da151526d06d69c2e26080095159c5dcd9362c
Jason <SilvisDevelopment@Gmail.com>
2026-09-10 12:34:39 -0400

    Redone the layout for the procedures on the display list.

--- client/MyPAS/src/components/DisplayList.jsx
+++ client/MyPAS/src/components/DisplayList.jsx
@@ -36,22 +36,29 @@ export default function DisplayList({ items, type }) {
                 </Link>
               </div>
             ) : type === "procedure" ? (
-              <div className="flex justify-center items-center gap-4">
-                <p>
-                  <strong>ID:</strong> {item.id}
-                </p>
-                <p>
-                  <strong>Procedure:</strong> {item.procedureName}
-                </p>
-                <p>
-                  <strong>Date:</strong> {item.procedureDate}
-                </p>
-                <p>
-                  <strong>CPT Code:</strong> {item.cptCode}
-                </p>
-                <p>
-                  <strong>Cost: </strong> ${item.patientChargedAmount}
-                </p>
+              <div className="grid grid-cols-[1fr_2fr_1fr_1fr_1fr_auto] gap-4 items-center">
+                <div>
+                  <strong className="block">ID:</strong>
+                  <span className="break-words">{item.id}</span>
+                </div>
+                <div>
+                  <strong className="block">Name:</strong>
+                  <span className="break-words">{item.procedureName}</span>
+                </div>
+                <div>
+                  <strong className="block">Date:</strong>
+                  <span className="break-words">{item.procedureDate}</span>
+                </div>
+                <div>
+                  <strong className="block">CPT Code:</strong>
+                  <span className="break-words">{item.cptCode}</span>
+                </div>
+                <div>
+                  <strong className="block">Cost:</strong>
+                  <span className="break-words">
+                    ${item.patientChargedAmount}
+                  </span>
+                </div>
                 <Link
                   to={`/procedure/${item.id}`}
                   className="border border-white rounded-lg p-1 hover:bg-green-500"
