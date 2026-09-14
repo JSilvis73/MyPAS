d941a444cad197dad7e2d6ca078d164e738ba10c
Jason <SilvisDevelopment@Gmail.com>
2026-09-14 17:02:04 -0400

    Reworked the home page component to be more responsive.

--- client/MyPAS/src/pages/HomePage.jsx
+++ client/MyPAS/src/pages/HomePage.jsx
@@ -5,25 +5,39 @@ import { IoCreateOutline } from "react-icons/io5";
 import { FaTasks } from "react-icons/fa";
 import { useAuth } from "../context/AuthContext";
 
-
 export default function HomePage() {
-  const auth = useAuth();
-  console.log("HomePage user:", auth.user);
-  return (
-    <div className="m-4 w-3xl max-w-6xl mx-auto bg-gray-800 text-white text-center border border-gray-600 rounded-lg p-4 shadow-lg">
-    
-      <h2 className="text-2xl mb-4"><strong>Home</strong></h2>
-      <p>Welcome {auth.user?.userName}</p>
-     
-        <div className="flex justify-center flex-wrap space-x-4 gap-4 mt-4">
-          <Card props={{route: "/search", name: "Search", img: <CiSearch className="h-20 w-20" />}} />
-          <Card props={{route: "/add-patient", name: "Create", img: <IoCreateOutline className="h-20 w-20"/> }} />
-          <Card props={{route: "/operations", name: "Operations", img: <FaTasks className="h-18 w-18"/> }} />        
-        </div>
-     
-      
+  const { user } = useAuth();
 
+  return (
+    <div className="flex flex-col text-white items-center justify-content p-4">
+      <h2 className="text-2xl mb-4">
+        <strong>Home</strong>
+      </h2>
+      <p>Welcome {user?.userName}</p>
+
+      <div className="flex justify-center text-center flex-wrap space-x-4 gap-4 mt-4">
+        <Card
+          props={{
+            route: "/search",
+            name: "Search",
+            img: <CiSearch className="h-20 w-20" />,
+          }}
+        />
+        <Card
+          props={{
+            route: "/add-patient",
+            name: "Create",
+            img: <IoCreateOutline className="h-20 w-20" />,
+          }}
+        />
+        <Card
+          props={{
+            route: "/operations",
+            name: "Operations",
+            img: <FaTasks className="h-18 w-18" />,
+          }}
+        />
+      </div>
     </div>
-    
   );
 }
